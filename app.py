890a9a8c45483b08801cb5af13ceed481c3005e8
Esteban Mendoza <estmendoza@ucla.edu>
2021-10-30 20:01:44 -0700

    update authorization code flow

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, redirect, url_for, session, request
 import os
-import spotipy, time
-from spotipy.oauth2 import SpotifyOAuth
+import spotipy
 from secrets import *
+from flask_session import Session
+import uuid
+
 
 app = Flask(__name__) 
 
@@ -19,38 +21,56 @@ def sorter():
 def results():
     return render_template('results.html')
 
-
-app.secret_key = os.environ['SECRET_KEY']
-app.config['SESSION_COOKIE_NAME'] = 'RankMee Cookie'
-TOKEN_INFO = 'token_info'
+"""
+*    Title: Spotipy examples
+*    Author: Lamere, Paul
+*    Date: Jan 30
+*    Code version: 
+*    Availability: https://github.com/plamere/spotipy/blob/0a9270f3853774d60210a66da66496684422b559/examples/app.py
+"""
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = '.flask_session/'
 
 clientid = os.environ['CLIENT_ID']
-clientsecret = os.environ['CLIENT_SECRET']
+clientsecret = os.environ['CLIENT_ID']
 
-@app.route('/SpotifyLogin')
-def login():
-    sp_oauth = create_spotify_oauth()
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)
+Session(app)
 
-@app.route('/redirect')
-def redirectPage():
-    sp_oauth = create_spotify_oauth()
-    session.clear()
-    code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code)
-    session[TOKEN_INFO] = token_info 
-    return redirect(url_for('spotifyranks', _external=True))
+caches_folder = '.spotify_caches/'
+if not os.path.exists(caches_folder):
+    os.makedirs(caches_folder)
+
+def session_cache_path():
+    return caches_folder + session.get('uuid')
+
 
 @app.route('/spotifyranks')
 def spotifyranks():
-    try:
-        token_info = get_token()
-    except:
-        print('user not logged in')
-        return redirect(url_for('login', _external=True))
-    
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+    if not session.get('uuid'):
+        # Step 1. Visitor is unknown, give random ID
+        session['uuid'] = str(uuid.uuid4())
+
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-library-read user-top-read', 
+                                                client_id=clientid,
+                                                client_secret = clientsecret,
+                                                redirect_uri=url_for('spotifyranks', _external=True),
+                                                cache_handler=cache_handler, 
+                                                show_dialog=True)
+
+    if request.args.get("code"):
+        # Step 3. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/spotifyranks')
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        # Step 2. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return redirect(auth_url)
+
+    # Step 4. Signed in, display data
+    sp = spotipy.Spotify(auth_manager=auth_manager)
 
     def top10():
         names = []
@@ -78,31 +98,12 @@ def spotifyranks():
     items = sp.current_user_top_tracks(limit=5, offset=0, time_range='short_term')['items']
     top10()
     songs_short = top_list
-
+    
     data = {"artists_long": artists_long, "artists_medium": artists_medium, "artists_short": artists_short, "songs_long": songs_long, "songs_medium": songs_medium, "songs_short": songs_short}
 
     return render_template('spotifyranks.html', data=data)
 
 
-def get_token():
-    token_info = session.get(TOKEN_INFO, None)
-    if not token_info:
-        raise 'exception'
-    now = int(time.time())
-    is_expired = token_info['expires_at'] - now <60
-    if (is_expired):
-        sp_oauth = create_spotify_oauth()
-        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
-    return token_info
-    
-
-def create_spotify_oauth():
-    return SpotifyOAuth(
-        client_id = clientid,
-        client_secret = clientsecret,
-        redirect_uri = url_for('redirectPage', _external=True),
-        scope = 'user-library-read user-top-read'
-    )
 
 
 if __name__ == '__main__':
